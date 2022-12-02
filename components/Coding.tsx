c23f0cf5f9450c8b008e3cc16b0cd7cf62012d87
mtrentz <mateus.trentz@gmail.com>
2022-12-02 16:40:17 -0300

    prog

--- components/Coding.tsx
+++ components/Coding.tsx
@@ -28,43 +28,43 @@ const Coding = () => {
     const content: CodingContent = [
         {
             title: "Chess Image Recognizer",
-            description: <p>Project that detects a chess board from a screenshot and lets you continue playing the match on Lichess. Coded on {tech.python} with the machine learning model made on {tech.pytorch}, web app built with {tech.django} and distributed with {tech.docker}.</p>,
+            description: <span>Project that detects a chess board from a screenshot and lets you continue playing the match on Lichess. Coded on {tech.python} with the machine learning model made on {tech.pytorch}, web app built with {tech.django} and distributed with {tech.docker}.</span>,
             link: "https://github.com/mtrentz/Chess-Recognizer-App",
             image: <Image src={chess} alt="Chessboard" layout="fill" />,
         },
         {
             title: "Plain Text Tetris",
-            description: <p>Website to play Tetris with plain text, using only ASCII characters. Built with {tech.react}, all the logic was coded in {tech.javascript}. Try the game live on <a target="_blank" className="underline text-link-600 hover:text-link-800 visited:text-visited-600" rel="noreferrer" href="https://tetris.mtrentz.com.br">this website</a>.</p>,
+            description: <span>Website to play Tetris with plain text, using only ASCII characters. Built with {tech.react}, all the logic was coded in {tech.javascript}. Try the game live on <a target="_blank" className="underline text-link-600 hover:text-link-800 visited:text-visited-600" rel="noreferrer" href="https://tetris.mtrentz.com.br">this website</a>.</span>,
             link: "https://github.com/mtrentz/Plain-Text-Tetris",
             image: <Image src={tetris} alt="Tetris" layout="fill" />,
         },
         {
             title: "Animations with p5.js",
-            description: <p>I&apos;ve made quite a few interactive animations with p5.js, which is a {tech.javascript} library for creative coding.</p>,
+            description: <span>I&apos;ve made quite a few interactive animations with p5.js, which is a {tech.javascript} library for creative coding.</span>,
             link: "https://github.com/mtrentz/p5_projects",
             image: <Image src={p5} alt="Tetris" layout="fill" />,
         },
         {
             title: "Stocks Porftolio Analysis",
-            description: <p>One of my first coding projects. Portfolio value tracker using public APIs that generates graphs of value progression, sends emails weekly about earnings. Coded on {tech.python}, mostly using {tech.pandas} and {tech.matplotlib}.</p>,
+            description: <span>One of my first coding projects. Portfolio value tracker using public APIs that generates graphs of value progression, sends emails weekly about earnings. Coded on {tech.python}, mostly using {tech.pandas} and {tech.matplotlib}.</span>,
             link: "https://github.com/mtrentz/Bolsa",
             image: <Image src={bolsa} alt="Tetris" layout="fill" />,
         },
         {
             title: "This Website",
-            description: <p>Curriculum website built with {tech.react}, {tech.nextjs} and {tech.tailwind} for styling. Deployed with {tech.docker}.</p>,
+            description: <span>Curriculum website built with {tech.react}, {tech.nextjs} and {tech.tailwind} for styling. Deployed with {tech.docker}.</span>,
             link: "https://github.com/mtrentz/Bolsa",
             image: <Image src={react} alt="Tetris" layout="fill" />,
         },
         {
             title: "Imgur Text Scraper",
-            description: <p>A imgur random image scraper that also detects the text in the image. Built with both {tech.python} and {tech.golang}, using a library called {tech.easyocr} for text detection. Uses {tech.fastapi} and was deployed with {tech.docker}.</p>,
+            description: <span>A imgur random image scraper that also detects the text in the image. Built with both {tech.python} and {tech.golang}, using a library called {tech.easyocr} for text detection. Uses {tech.fastapi} and was deployed with {tech.docker}.</span>,
             link: "https://github.com/mtrentz/Imgur-Text-Scraper",
             image: <Image src={imgur} alt="Tetris" layout="fill" />,
         },
         {
             title: "Brazillian Companies Federal Revenue Data",
-            description: <p>Program that downloads, cleans and imports the federal revenue registration data for all companies in Brazil to a MySQL database in a Docker container. Built with {tech.golang}, working with {tech.docker}.</p>,
+            description: <span>Program that downloads, cleans and imports the federal revenue registration data for all companies in Brazil to a MySQL database in a Docker container. Built with {tech.golang}, working with {tech.docker}.</span>,
             link: "https://github.com/mtrentz/dados-cnpj",
             image: <Image src={gov} alt="Tetris" layout="fill" />,
         },
@@ -78,7 +78,7 @@ const Coding = () => {
                 {/* Loop over content */}
                 {content.map((item, index) => {
                     return (
-                        <>
+                        <div key={index}>
                             <CardCoding
                                 title={item.title}
                                 description={item.description}
@@ -86,7 +86,6 @@ const Coding = () => {
                                 link={item.link}
                                 // It goes from right to left, alternating
                                 imageOnLeft={index % 2 === 0 ? true : false}
-                                key={index}
                             />
                             {/* If index is not last, return also a separator */}
                             {index !== content.length - 1 ?
@@ -94,7 +93,7 @@ const Coding = () => {
                                     <div className="bg-lightPrimary-300 dark:bg-darkPrimary-700 w-11/12 h-px"></div>
                                 </div>
                                 : null}
-                        </>
+                        </div>
                     )
                 })}
             </div>
